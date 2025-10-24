64621b9a2ca80d89dc3ce7e2f914d3a9feff38b7
ORIOP25 <rodrigommanguinhas@gmail.com>
2025-10-24 17:35:08 +0100

    Adicionar ThemeProvider para corrigir estilos

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ThemeProvider } from "next-themes"; // Importa o ThemeProvider
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  // Adiciona o ThemeProvider aqui
+  <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <App />
+  </ThemeProvider>
+);
